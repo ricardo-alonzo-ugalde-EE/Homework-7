a5ea1134eca638d945e6beecea46aa33d30335b6
Ricardo Alonzo Ugalde <ricardo.alonzo.ugalde@gmail.com>
2020-05-24 22:20:52 -0700

    Question 2 finished

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ void listNodePrint(listNode* node_element)
 		printf("%d\n", node_element->node_value);
 		node_element = node_element->next_node;
 	}
+	printf("\n");
 }
 
 // Function to delete every other element from a singly linked list
@@ -42,6 +43,48 @@ void deleteNodeElement(listNode *head)
 Question 2: Implement a stack (stack structure, push function, pop function) using an array.
 */
 
+#define SIZE 5 // stack size defintion for array based stack
+int stack[SIZE]; // stack size 5
+int top = -1; // stack not yet initialized so make number negative for now
+
+// function to print popped elements from the stack
+//void printArrayStack()
+//{
+//	printf("ok this prints");
+//	int i;
+//	for (i = 0; i <= 3; i++)
+//	{
+//		printf("%d\n", stack[i]);
+//	}
+//}
+//function to push elements to the stack
+void pushElement(int element_value)
+{
+	if (top < 0) /*if top is neg, then add element value to first 
+					 array position (index) and make top 0*/
+		{
+			stack[0] = element_value;
+			top = 0;
+			printf("\nThe element value of %d has been pushed to the index of the array stack.\n", element_value);
+		}
+		else /* if there is an element in index, move over top + n*/
+		{
+			stack[top + 1] = element_value;
+			top++ ;
+			printf("\nThe element value of %d has been pushed to the array stack.\n", element_value);
+		}
+	
+}
+
+//function to pop elements from the stack
+void popElement()
+{
+	int x = stack[top];
+	printf("\nThe element with the value of %d has been popped from the Array Stack\n", stack[top]);
+	top = top - 1;
+	
+	
+}
 
 int main()
 {
@@ -67,7 +110,18 @@ Question 1 main:
 	fifth->next_node = NULL;
 	listNodePrint(head); // prints nodes before delete
 	deleteNodeElement(head); // call to the delete Node function
-	
+/*
+Question 2 main:
+*/
+	pushElement(4);
+	pushElement(6);
+	popElement();
+	pushElement(8);
+	pushElement(7);
+	pushElement(10);
+	popElement();
+	//printArrayStack();
 
-	return 0;
+
+	return 0;;
 }
